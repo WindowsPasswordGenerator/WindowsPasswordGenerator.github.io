89c134e485f30fa85cb2c9762dad64c1952f589c
Yurii Antonchyk <xsat.php@gmail.com>
2023-12-27 21:04:00 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Windows Password Generator
+
---
+++ js/main.js
@@ -0,0 +1,60 @@
+const DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const UPPER_LETTERS = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+const LOWER_LETTERS = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+const SPECIAL_CHARACTERS = [
+    '\\', '-', '!', '"', '#', '$', '%', '&', '(', ')', '*', ',', '.',
+    '/', ':', ';', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}',
+    '~', '+', '<', '=', '>',
+];
+
+function getRandKey(elements) {
+	return Math.floor(Math.random() * elements.length);
+}
+
+function getRandElement(elements) {
+	const random_key = getRandKey(elements);
+
+	return elements[random_key];
+}
+
+function getRandHash(size, elements) {
+	let hash = '';
+
+	for (let i = 0; i < size; i++) {
+		hash += getRandElement(elements);
+	}
+
+	return hash;
+}
+
+function generateRandomPassword(size_input_id) {
+	const size = document.getElementById(size_input_id).value;
+    const elements = [].concat(DIGITS, UPPER_LETTERS, LOWER_LETTERS,SPECIAL_CHARACTERS)
+
+	alert(getRandHash(size, elements));
+}
+
+function generateComplexPassword(size_input_id) {
+	const size = document.getElementById(size_input_id).value;
+	const element_map = [DIGITS, UPPER_LETTERS, LOWER_LETTERS, SPECIAL_CHARACTERS];
+    let temp_element_map = [...element_map];
+    let hash = '';
+
+    for (let i = 0; i < size; i++) {
+        if (temp_element_map.length === 0) {
+            temp_element_map = [...element_map];
+        }
+
+        const random_key = getRandKey(temp_element_map);
+        hash += getRandElement(temp_element_map[random_key]);
+        temp_element_map.splice(random_key, 1);
+    }
+
+	alert(hash);
+}
